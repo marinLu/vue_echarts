d16ea8da83b6ead02fd14ad1d7c2f096447b1dbd
marin <1198967717@qq.com>
2017-08-20 21:46:59 +0800

    GIS系统

---
+++ src/packages/M1.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <echarts-component :option='option'></echarts-component>
+  </div>
+</template>
+<script>
+import echarts from 'echarts'
+import echartsComponent from '../components/Echarts'
+
+export default {
+    name:'m1',
+    data() {
+        var option=[];
+        return {
+          msg1:'hello',
+            chart: null,
+            option:this.$store.state.m1,
+        }
+    },
+    components:{
+      echartsComponent
+    },
+    methods:{
+      eRender(){
+            for(let i=0,len=this.option.length;i<len;i++){
+            this.chart = echarts.init(document.querySelector(`.a${i+1}`));
+            this.chart.setOption(this.option[i]);
+        }
+      }
+    },
+    mounted() {
+        this.$nextTick(
+            this.eRender
+      )
+    }
+}
+</script>
